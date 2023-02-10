170bff105054267e0f9e01f2b64185d16885766d
Amna Azam <71166016+Amnaazam123@users.noreply.github.com>
2023-02-10 16:17:27 +0500

    Create 16. assertRegex.py

---
+++ 16. assertRegex.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestMathFunctions(unittest.TestCase):
+    def test_sqrt(self):
+        self.assertRegex("Amna","^[A-Z]mna")
+
+
+if __name__=="__main__":
+    unittest.main()
